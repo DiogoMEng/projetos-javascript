0018614d97429b4131161532f17d36d1529e21dd
DiogoMEng <diogoeng19@gmail.com>
2024-10-25 18:55:05 -0300

    :sparkles: feat: Criação de funcionalidades de filtro

--- Tougths/src/controllers/ToughtController.js
+++ Tougths/src/controllers/ToughtController.js
@@ -1,11 +1,43 @@
 const Tought = require("../models/Tougth.js");
 const User = require("../models/User.js");
 
+const { Op } = require("sequelize");
+
 module.exports = class ToughController {
   
   static async showToughts(req, res) {
+
+    let search = "";
+
+    if(req.query.search) {
+      search = req.query.search;
+    }
+
+    let order = "DESC";
+
+    if (req.query.order === "old") {
+      order = "ASC";
+    } else {
+      order = "DESC";
+    }
+
+    const toughtsData = await Tought.findAll({
+      include: User,
+      where: {
+        title: {
+          [Op.like]: `%${search}%`
+        }
+      },
+      order: [['createdAt', order]]
+    });
+
+    const toughts = toughtsData.map((result) => result.get({ plain: true }));
+
+    let toughtsQty = toughts.length;
+
+    if(toughtsQty === 0) toughtsQty = false;
     
-    res.render('toughts/home')
+    res.render('toughts/home', { toughts, search, toughtsQty });
 
   }
 
@@ -28,7 +60,7 @@ module.exports = class ToughController {
     let emptyToughts = true;
 
     if (toughts.length > 0) {
-      let emptyToughts = false;
+      emptyToughts = false;
     }
  
     res.render('toughts/dashboard', { toughts, emptyToughts });
@@ -85,11 +117,38 @@ module.exports = class ToughController {
 
     const id = req.params.id;
 
-    const tought = await Tought.findOne({ where: {
-      id: id
-    } });
+    const tought = await Tought.findOne({ 
+      where: {
+        id: id
+      },
+      raw: true
+    });
 
-    res.render('/toughts/edit', { tought });
+    res.render('toughts/edit', { tought });
+
+  }
+
+  static async updateToughtSave(req, res) {
+
+    const id = req.body.id;
+    const tought = {
+      title: req.body.title
+    }
+
+    
+    try {
+      await Tought.update(tought, { 
+        where: { id: id } 
+      });
+
+      req.flash('message', 'Pensamento atualizado com sucesso!');
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard');
+      });
+    } catch(error) {
+      console.log(error);
+    }
 
   }
 
